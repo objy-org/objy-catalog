66649d093c46e0b3c0ffb4c38ee244770cf62d20
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-07-01 16:15:05 +0200

    initial

---
+++ mappers/observers/intervalMapper.js
@@ -0,0 +1,71 @@
+var moment = require("moment");
+
+Mapper = function(OBJY) {
+    return Object.assign(new OBJY.ObserverTemplate(OBJY), {
+
+        initialize: function(millis) {
+            var self = this;
+
+            this.interval = setInterval(function() {
+
+                self.run(moment().utc());
+
+            }, this.interval)
+        },
+
+        run: function(date) {
+
+            var self = this;
+
+            OBJY.getPersistence(self.objectFamily).listClients(function(data) {
+
+                data.forEach(function(tenant) {
+
+                    OBJY.Logger.log("Running ovserver " + date.toISOString() + " " + tenant)
+
+                    OBJY.getPersistence(self.objectFamily).getByCriteria({
+                        _aggregatedEvents: {
+                            $elemMatch: {
+                                'date': {
+                                    $lte: date.toISOString()
+                                }
+                            }
+                        }
+                    }, function(objs) {
+
+                        objs.forEach(function(obj) {
+
+                            obj = OBJY[self.objectFamily](obj);
+
+                            obj._aggregatedEvents.forEach(function(aE) {
+
+                                var prop = obj.getProperty(aE.propName);
+
+                                OBJY.execProcessorAction(prop.action, obj, prop, null, function() {
+
+                                    obj.setEventTriggered(aE.propName, true, tenant).update(function(d) {
+
+                                        OBJY.Logger.log('d._aggregatedEvents')
+                                        OBJY.Logger.log(d._aggregatedEvents)
+
+                                    }, function(err) {
+                                        console.log(err);
+                                    }, tenant)
+
+                                }, tenant, {});
+                            })
+                        })
+
+                    }, function(err) {
+
+                    }, /*app*/ undefined, tenant, {})
+                })
+
+            }, function(err) {
+
+            })
+        }
+    })
+}
+
+module.exports = Mapper;
---
+++ mappers/processors/eval.js
@@ -0,0 +1,26 @@
+Mapper = function(OBJY) {
+    return Object.assign(new OBJY.ProcessorTemplate(OBJY), {
+
+        execute: function(dsl, obj, prop, data, callback, client, app, user, options) {
+
+            OBJY.Logger.log("Executing dsl in mapper")
+            if (this.multitenancy == this.CONSTANTS.MULTITENANCY.ISOLATED) {
+                try {
+                    eval(dsl);
+                } catch (e) {
+                    OBJY.Logger.error(e)
+                }
+                callback();
+            } else {
+                try {
+                    eval(dsl);
+                } catch (e) {
+                    OBJY.Logger.error(e)
+                }
+                callback();
+            }
+        }
+    })
+}
+
+module.exports = Mapper;
